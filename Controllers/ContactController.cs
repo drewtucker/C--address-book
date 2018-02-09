9aca0b09068686e6696c53515a35933b0818ba5e
Drew Tucker <dtuck43@gmail.com>
2018-02-09 11:42:36 -0800

    Add Contact controller with Index route.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using AddressBook.Models;
+namespace AddressBook.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
