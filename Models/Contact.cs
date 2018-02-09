f93fca3575b82272e686ecbd6851e1a94977f3da
Drew Tucker <dtuck43@gmail.com>
2018-02-09 11:12:05 -0800

    Add Contact class with class constructor.

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -7,5 +7,14 @@ namespace AddressBook.Models
     private int _phoneNumber;
     private Address _address;
     private static List<Contact> _contactInstances = new List<Contact> {};
+
+    public Contact(string name, int phoneNumber, Address address)
+    {
+      _name = name;
+      _phoneNumber = phoneNumber;
+      _address = address;
+    }
+
+    
   }
 }
